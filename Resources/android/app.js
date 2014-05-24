0748599c3e1b56ead14d08822fda1c172e32ec5b
joethefox <joethefox@gmail.com>
2014-05-24 20:19:48 +0200

    remove resources folder

--- Resources/android/app.js
+++
@@ -1,8 +0,0 @@
-var Alloy = require("alloy"), _ = Alloy._, Backbone = Alloy.Backbone;
-
-Alloy.Globals.DefaultEnemy = {
-    TYPE: "normal",
-    POINT: 1,
-    IMAGE: "/images/enemy.png",
-    NAME: "PIPPO-JOE-LSD"
-}, Alloy.createController("index");
