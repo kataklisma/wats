64b3ca2d95433d043ddc08d9e7b20ab4fead332b
Enrico Deleo <enrico@lysergicstudio.com>
2014-05-25 00:27:11 +0200

    aggiunti assets splash, menu e in-game. Creata vista menu iniziale.

--- Resources/android/alloy/controllers/index.js
+++ Resources/android/alloy/controllers/index.js
@@ -1,23 +1,4 @@
 function Controller() {
-    function start() {
-        createEnemies();
-        Alloy.createController("ar", {
-            enemies: Alloy.Collections.Enemy
-        }).getView().open();
-    }
-    function createEnemies() {
-        var enemy = Alloy.createModel("Enemy", {
-            name: Alloy.Globals.DefaultEnemy.NAME,
-            lat: "37.50942942",
-            lon: "15.08363915",
-            type: Alloy.Globals.DefaultEnemy.TYPE,
-            point: Alloy.Globals.DefaultEnemy.POINT,
-            image: Alloy.Globals.DefaultEnemy.IMAGE,
-            status: 0
-        });
-        enemy.save();
-        Alloy.Collections.Enemy.add(enemy);
-    }
     require("alloy/controllers/BaseController").apply(this, Array.prototype.slice.call(arguments));
     this.__controllerPath = "index";
     arguments[0] ? arguments[0]["__parentSymbol"] : null;
@@ -25,22 +6,24 @@ function Controller() {
     arguments[0] ? arguments[0]["__itemTemplate"] : null;
     var $ = this;
     var exports = {};
-    var __defers = {};
     $.__views.index = Ti.UI.createWindow({
-        backgroundColor: "white",
+        backgroundColor: "black",
         id: "index"
     });
     $.__views.index && $.addTopLevelView($.__views.index);
-    $.__views.start = Ti.UI.createButton({
-        id: "start",
-        title: "start"
+    $.__views.home - menu = Ti.UI.createView({
+        id: "home-menu"
     });
-    $.__views.index.add($.__views.start);
-    start ? $.__views.start.addEventListener("click", start) : __defers["$.__views.start!click!start"] = true;
+    $.__views.index.add($.__views.home - menu);
+    $.__views.home - menu - easy = Ti.UI.createButton({
+        id: "home-menu-easy",
+        title: "EASY"
+    });
+    $.__views.home - menu.add($.__views.home - menu - easy);
     exports.destroy = function() {};
     _.extend($, $.__views);
+    alert("ciao");
     $.index.open();
-    __defers["$.__views.start!click!start"] && $.__views.start.addEventListener("click", start);
     _.extend($, exports);
 }
 
--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,25 +1,4 @@
-function start(e) {
-    createEnemies();
-    
-    Alloy.createController("ar", {
-        enemies : Alloy.Collections.Enemy
-    }).getView().open();
-}
 
-function createEnemies() {
-    var enemy = Alloy.createModel("Enemy", {
-        name : Alloy.Globals.DefaultEnemy.NAME,
-        lat : "37.50942942",
-        lon : "15.08363915",
-        type : Alloy.Globals.DefaultEnemy.TYPE,
-        point : Alloy.Globals.DefaultEnemy.POINT,
-        image : Alloy.Globals.DefaultEnemy.IMAGE,
-        status : 0
-    });
 
-    enemy.save();
-    Alloy.Collections.Enemy.add(enemy);
-
-}
 
 $.index.open();
